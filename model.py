4cb65c6bc8d2d75b8a6a99f4dd251a2d404064f7
NikitaSPBU <st111245@student.spbu.ru>
2022-11-22 22:12:45 +0300

    add site2

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
-from pathlib import Path
 import shutil
+from pathlib import Path
 
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 
 from config import SQLITE_DATABASE_NAME, SQLITE_DATABASE_BACKUP_NAME
-
 
 convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -20,29 +19,30 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Post(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(512), nullable=False)
     text = db.Column(db.String(2048), nullable=False)
 
-    created_on = db.Column(db.DateTime(timezone=True),
-                           server_default=db.func.now())
-    updated_on = db.Column(db.DateTime(
-        timezone=True), server_default=db.func.now(), server_onupdate=db.func.now())
+    created_on = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    updated_on = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), server_onupdate=db.func.now())
 
 
 def db_init():
-
-    guestbook_init = []
-
+    guestbook_init = [
+        {'name': 'Дмитрий', 'text': 'Это первая запись!'},
+        {'name': 'Дмитрий', 'text': 'Добавляйте свои отзывы!'},
+    ]
+    # Check if db file already exists. If so, backup it
     db_file = Path(SQLITE_DATABASE_NAME)
     if db_file.is_file():
         shutil.copyfile(SQLITE_DATABASE_NAME, SQLITE_DATABASE_BACKUP_NAME)
 
-    db.session.commit()
+    # Init DB
+    db.session.commit()  # https://stackoverflow.com/questions/24289808/drop-all-freezes-in-flask-with-sqlalchemy
     db.drop_all()
     db.create_all()
 
+    # Create TestOne questions
     print("Create guestbook")
     for g in guestbook_init:
         gb = Post(name=g['name'], text=g['text'])
